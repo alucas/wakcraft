0d6cdc1912dcf4ca1781228f3b6f032c91b31aa9
Antoine <antoine.lucas.33@gmail.com>
2014-04-14 16:06:32 +0200

    Fix ore rendering

--- src/main/java/heero/wakcraft/renderer/RenderBlockGeneric.java
+++ src/main/java/heero/wakcraft/renderer/RenderBlockGeneric.java
@@ -8,49 +8,60 @@ import org.lwjgl.opengl.GL11;
 
 import cpw.mods.fml.client.registry.ISimpleBlockRenderingHandler;
 
-public abstract class RenderBlockGeneric implements ISimpleBlockRenderingHandler {
-	public RenderBlockGeneric() {};
+public abstract class RenderBlockGeneric implements
+		ISimpleBlockRenderingHandler {
 
 	@Override
 	public void renderInventoryBlock(Block block, int metadata, int modelID,
 			RenderBlocks renderer) {
-		Tessellator tessellator = Tessellator.instance;
-		GL11.glTranslatef(-0.5F, -0.5F, -0.5F);
-		tessellator.startDrawingQuads();
-		tessellator.setNormal(0.0F, -1.0F, 0.0F);
-		renderer.renderFaceYNeg(block, 0.0D, 0.0D, 0.0D,
-				renderer.getBlockIconFromSideAndMetadata(block, 0, metadata));
-		tessellator.draw();
-		tessellator.startDrawingQuads();
-		tessellator.setNormal(0.0F, 1.0F, 0.0F);
-		renderer.renderFaceYPos(block, 0.0D, 0.0D, 0.0D,
-				renderer.getBlockIconFromSideAndMetadata(block, 1, metadata));
-		tessellator.draw();
-		tessellator.startDrawingQuads();
-		tessellator.setNormal(0.0F, 0.0F, -1.0F);
-		renderer.renderFaceZNeg(block, 0.0D, 0.0D, 0.0D,
-				renderer.getBlockIconFromSideAndMetadata(block, 2, metadata));
-		tessellator.draw();
-		tessellator.startDrawingQuads();
-		tessellator.setNormal(0.0F, 0.0F, 1.0F);
-		renderer.renderFaceZPos(block, 0.0D, 0.0D, 0.0D,
-				renderer.getBlockIconFromSideAndMetadata(block, 3, metadata));
-		tessellator.draw();
-		tessellator.startDrawingQuads();
-		tessellator.setNormal(-1.0F, 0.0F, 0.0F);
-		renderer.renderFaceXNeg(block, 0.0D, 0.0D, 0.0D,
-				renderer.getBlockIconFromSideAndMetadata(block, 4, metadata));
-		tessellator.draw();
-		tessellator.startDrawingQuads();
-		tessellator.setNormal(1.0F, 0.0F, 0.0F);
-		renderer.renderFaceXPos(block, 0.0D, 0.0D, 0.0D,
-				renderer.getBlockIconFromSideAndMetadata(block, 5, metadata));
-		tessellator.draw();
-		GL11.glTranslatef(0.5F, 0.5F, 0.5F);
+		renderInventoryStandardBlock(block, metadata, renderer, 1, 1, 1);
 	}
 
 	@Override
 	public boolean shouldRender3DInInventory(int modelId) {
 		return true;
+	}
+
+	protected void renderInventoryStandardBlock(Block block, int metadata,
+			RenderBlocks renderer, float colorR, float colorG, float colorB) {
+		Tessellator tessellator = Tessellator.instance;
+		GL11.glTranslatef(-0.5F, -0.5F, -0.5F);
+		tessellator.startDrawingQuads();
+		tessellator.setNormal(0.0F, -1.0F, 0.0F);
+		tessellator.setColorOpaque_F(colorR, colorG, colorB);
+		renderer.renderFaceYNeg(block, 0.0D, 0.0D, 0.0D,
+				renderer.getBlockIconFromSideAndMetadata(block, 0, metadata));
+		tessellator.draw();
+		tessellator.startDrawingQuads();
+		tessellator.setNormal(0.0F, 1.0F, 0.0F);
+		tessellator.setColorOpaque_F(colorR, colorG, colorB);
+		renderer.renderFaceYPos(block, 0.0D, 0.0D, 0.0D,
+				renderer.getBlockIconFromSideAndMetadata(block, 1, metadata));
+		tessellator.draw();
+		tessellator.startDrawingQuads();
+		tessellator.setNormal(0.0F, 0.0F, -1.0F);
+		tessellator.setColorOpaque_F(colorR, colorG, colorB);
+		renderer.renderFaceZNeg(block, 0.0D, 0.0D, 0.0D,
+				renderer.getBlockIconFromSideAndMetadata(block, 2, metadata));
+		tessellator.draw();
+		tessellator.startDrawingQuads();
+		tessellator.setNormal(0.0F, 0.0F, 1.0F);
+		tessellator.setColorOpaque_F(colorR, colorG, colorB);
+		renderer.renderFaceZPos(block, 0.0D, 0.0D, 0.0D,
+				renderer.getBlockIconFromSideAndMetadata(block, 3, metadata));
+		tessellator.draw();
+		tessellator.startDrawingQuads();
+		tessellator.setNormal(-1.0F, 0.0F, 0.0F);
+		tessellator.setColorOpaque_F(colorR, colorG, colorB);
+		renderer.renderFaceXNeg(block, 0.0D, 0.0D, 0.0D,
+				renderer.getBlockIconFromSideAndMetadata(block, 4, metadata));
+		tessellator.draw();
+		tessellator.startDrawingQuads();
+		tessellator.setNormal(1.0F, 0.0F, 0.0F);
+		tessellator.setColorOpaque_F(colorR, colorG, colorB);
+		renderer.renderFaceXPos(block, 0.0D, 0.0D, 0.0D,
+				renderer.getBlockIconFromSideAndMetadata(block, 5, metadata));
+		tessellator.draw();
+		GL11.glTranslatef(0.5F, 0.5F, 0.5F);
 	}
 }
--- src/main/java/heero/wakcraft/renderer/RenderBlockOre1.java
+++ src/main/java/heero/wakcraft/renderer/RenderBlockOre1.java
@@ -16,6 +16,30 @@ public class RenderBlockOre1 extends RenderBlockGeneric {
 	public int getRenderId() {
 		return renderId;
 	}
+	
+	@Override
+	public void renderInventoryBlock(Block block, int metadata, int modelID,
+			RenderBlocks renderer) {
+		if ((metadata & 1) == 0) {
+			float[] color = ((BlockOre1)block).getColor(metadata);
+			
+			renderer.setOverrideBlockTexture(BlockOre1.iconTop);
+			
+			renderer.setRenderBounds(0.125, 0.75, 0.125, 0.5, 0.875, 0.5);
+			renderInventoryStandardBlock(block, metadata, renderer, color[0], color[1], color[2]);
+			
+			renderer.setRenderBounds(0.25, 0.75, 0.5625, 0.4375, 0.8125, 0.75);
+			renderInventoryStandardBlock(block, metadata, renderer, color[0], color[1], color[2]);
+			
+			renderer.setRenderBounds(0.5625, 0.75, 0.375, 0.8125, 0.8125, 0.5625);
+			renderInventoryStandardBlock(block, metadata, renderer, color[0], color[1], color[2]);
+			
+			renderer.clearOverrideBlockTexture();
+		}
+
+		renderer.setRenderBounds(0, 0, 0, 1, 0.75, 1);
+        renderInventoryStandardBlock(block, metadata, renderer, 1, 1, 1);
+	}
 
 	@Override
 	public boolean renderWorldBlock(IBlockAccess blockAccess, int x, int y, int z,
@@ -24,24 +48,25 @@ public class RenderBlockOre1 extends RenderBlockGeneric {
 		int metadata = blockAccess.getBlockMetadata(x, y, z);
 		
 		if ((metadata & 1) == 0) {
+			float[] color = ((BlockOre1)block).getColor(metadata);
+			
 			renderer.setOverrideBlockTexture(BlockOre1.iconTop);
-			renderOre(x, y, z, block, renderer, ((BlockOre1)block).getColor(metadata));
+			renderer.renderAllFaces = true;
+			
+			renderer.setRenderBounds(0.125, 0.75, 0.125, 0.5, 0.875, 0.5);
+			renderer.renderStandardBlockWithColorMultiplier(block, x, y, z, color[0], color[1], color[2]);
+			
+			renderer.setRenderBounds(0.25, 0.75, 0.5625, 0.4375, 0.8125, 0.75);
+			renderer.renderStandardBlockWithColorMultiplier(block, x, y, z, color[0], color[1], color[2]);
+			
+			renderer.setRenderBounds(0.5625, 0.75, 0.375, 0.8125, 0.8125, 0.5625);
+			renderer.renderStandardBlockWithColorMultiplier(block, x, y, z, color[0], color[1], color[2]);
+			
+			renderer.renderAllFaces = false;
 			renderer.clearOverrideBlockTexture();
 		}
 
 		renderer.setRenderBounds(0, 0, 0, 1, 0.75, 1);
         return renderer.renderStandardBlock(block, x, y, z);
-	}
-	
-	public void renderOre(int x, int y, int z,
-			Block block, RenderBlocks renderer, float[] color) {
-		renderer.setRenderBounds(0.125, 0.75, 0.125, 0.5, 0.875, 0.5);
-		renderer.renderStandardBlockWithColorMultiplier(block, x, y, z, color[0], color[1], color[2]);
-		
-		renderer.setRenderBounds(0.25, 0.75, 0.5625, 0.4375, 0.8125, 0.75);
-		renderer.renderStandardBlockWithColorMultiplier(block, x, y, z, color[0], color[1], color[2]);
-		
-		renderer.setRenderBounds(0.5625, 0.75, 0.375, 0.8125, 0.8125, 0.5625);
-		renderer.renderStandardBlockWithColorMultiplier(block, x, y, z, color[0], color[1], color[2]);
 	}
 }
