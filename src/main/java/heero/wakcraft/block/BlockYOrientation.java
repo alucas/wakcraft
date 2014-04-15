3f2eeba8c9ad25be64368ceaed3617a3dcd17d22
Antoine <antoine.lucas.33@gmail.com>
2014-04-15 11:33:06 +0200

    Improve BlockYOrientation top icon rendering

--- src/main/java/heero/wakcraft/block/BlockYOrientation.java
+++ src/main/java/heero/wakcraft/block/BlockYOrientation.java
@@ -14,120 +14,111 @@ import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
 public abstract class BlockYOrientation extends Block {
-	protected boolean isCorner = false;
-	protected boolean isCenter = false;
-	
 	public BlockYOrientation(Material material) {
 		super(material);
 	}
-	
-    /**
-     * If this block doesn't render as an ordinary block it will return False (examples: signs, buttons, stairs, etc)
-     */
-    @Override
-    public boolean renderAsNormalBlock()
-    {
-        return false;
-    }
-    
-    /**
-     * The type of render function that is called for this block
-     */
-    @Override
-    public int getRenderType()
-    {
-        return RenderBlockYOrientation.renderId;
-    }
-    
-    /**
-     * Determines the damage on the item the block drops. Used in cloth and wood.
-     */
-    @Override
-    public int damageDropped(int metadata)
-    {
-        return metadata & 12;
-    }
-    
-    /**
-     * Gets the block's texture. Args: side, meta
-     */
-    @SideOnly(Side.CLIENT)
-    @Override
+
+	/**
+	 * If this block doesn't render as an ordinary block it will return False
+	 * (examples: signs, buttons, stairs, etc)
+	 */
+	@Override
+	public boolean renderAsNormalBlock() {
+		return false;
+	}
+
+	/**
+	 * The type of render function that is called for this block
+	 */
+	@Override
+	public int getRenderType() {
+		return RenderBlockYOrientation.renderId;
+	}
+
+	/**
+	 * Determines the damage on the item the block drops. Used in cloth and
+	 * wood.
+	 */
+	@Override
+	public int damageDropped(int metadata) {
+		return metadata & 12;
+	}
+
+	@Override
+	@SideOnly(Side.CLIENT)
+	public IIcon getIcon(IBlockAccess world, int x, int y, int z, int side) {
+		switch (side) {
+		case 0:
+		case 1:
+			int blockId = Block.getIdFromBlock(world.getBlock(x, y, z));
+			boolean t1 = Block.getIdFromBlock(world.getBlock(x + 1, y, z)) == blockId;
+			boolean t2 = Block.getIdFromBlock(world.getBlock(x, y, z + 1)) == blockId;
+			boolean t3 = Block.getIdFromBlock(world.getBlock(x - 1, y, z)) == blockId;
+			boolean t4 = Block.getIdFromBlock(world.getBlock(x, y, z - 1)) == blockId;
+
+			int neighbor = (t1 ? 1 : 0) + (t2 ? 2 : 0) + (t3 ? 4 : 0)
+					+ (t4 ? 8 : 0);
+
+			if (neighbor == 15) {
+				return getCenterIcon(side, 0);
+			} else if (neighbor == 12 || neighbor == 9 || neighbor == 03
+					|| neighbor == 6) {
+				return getCornerIcon(side, 0);
+			}
+
+			return getTopIcon(side, 0);
+		default:
+			return getSideIcon(side, 0);
+		}
+	}
+
+	/**
+	 * Gets the block's texture. Args: side, meta
+	 */
+	@SideOnly(Side.CLIENT)
+	@Override
 	public IIcon getIcon(int side, int metadata) {
 		switch (side) {
 		case 0:
 		case 1:
-			if (isCenter) {
-				return getCenterIcon(side, metadata);
-			} else if (isCorner) {
-				return getCornerIcon(side, metadata);
-			}
-
 			return getTopIcon(side, metadata);
 		default:
 			return getSideIcon(side, metadata);
 		}
 	}
-    
-	@SideOnly(Side.CLIENT)
-    public abstract IIcon getTopIcon(int side, int metadata);
-    
-	@SideOnly(Side.CLIENT)
-    public abstract IIcon getSideIcon(int side, int metadata);
-    
-	@SideOnly(Side.CLIENT)
-    public IIcon getCornerIcon(int side, int metadata) {
-    	return getTopIcon(side, metadata);
-    }
-	
-	@SideOnly(Side.CLIENT)
-    public IIcon getCenterIcon(int side, int metadata) {
-    	return getTopIcon(side, metadata);
-    }
-	
-	@Override
-	public void setBlockBoundsBasedOnState(IBlockAccess world, int x, int y,
-			int z) {
-		super.setBlockBoundsBasedOnState(world, x, y, z);
 
-		BlockYOrientation pillarBlock = (BlockYOrientation) world.getBlock(x, y, z);
-		int blockId = Block.getIdFromBlock(pillarBlock);
-		boolean t1 = Block.getIdFromBlock(world.getBlock(x + 1, y, z)) == blockId;
-		boolean t2 = Block.getIdFromBlock(world.getBlock(x, y, z + 1)) == blockId;
-		boolean t3 = Block.getIdFromBlock(world.getBlock(x - 1, y, z)) == blockId;
-		boolean t4 = Block.getIdFromBlock(world.getBlock(x, y, z - 1)) == blockId;
+	@SideOnly(Side.CLIENT)
+	public abstract IIcon getTopIcon(int side, int metadata);
 
-		int neighbor = (t1 ? 1 : 0) + (t2 ? 2 : 0) + (t3 ? 4 : 0)
-				+ (t4 ? 8 : 0);
+	@SideOnly(Side.CLIENT)
+	public abstract IIcon getSideIcon(int side, int metadata);
 
-		if (neighbor == 15) {
-			isCenter = true;
-			isCorner = false;
-		} else if (neighbor == 12 || neighbor == 9
-				|| neighbor == 03 || neighbor == 6) {
-			isCenter = false;
-			isCorner = true;
-		} else {
-			isCorner = false;
-			isCenter = false;
-		}
+	@SideOnly(Side.CLIENT)
+	public IIcon getCornerIcon(int side, int metadata) {
+		return getTopIcon(side, metadata);
 	}
-    
+
+	@SideOnly(Side.CLIENT)
+	public IIcon getCenterIcon(int side, int metadata) {
+		return getTopIcon(side, metadata);
+	}
+
 	/**
-     * Called when the block is placed in the world.
-     */
+	 * Called when the block is placed in the world.
+	 */
 	@Override
-    public void onBlockPlacedBy(World world, int x, int y, int z, EntityLivingBase player, ItemStack itemBlock)
-    {
+	public void onBlockPlacedBy(World world, int x, int y, int z,
+			EntityLivingBase player, ItemStack itemBlock) {
 		super.onBlockPlacedBy(world, x, y, z, player, itemBlock);
-		
+
 		setOrientationFromYaw(world, x, y, z, player.rotationYaw);
 	}
-    
-    public static void setOrientationFromYaw(World world, int x, int y, int z, float yaw){
-        int l = MathHelper.floor_double((double)(yaw * 4.0F / 360.0F) + 0.5D) & 3;
-        
-        switch (l) {
+
+	public static void setOrientationFromYaw(World world, int x, int y, int z,
+			float yaw) {
+		int l = MathHelper.floor_double((double) (yaw * 4.0F / 360.0F) + 0.5D) & 3;
+
+		switch (l) {
 		case 0:
 			setOrientation(world, x, y, z, ForgeDirection.SOUTH);
 			break;
@@ -140,13 +131,13 @@ public abstract class BlockYOrientation extends Block {
 		default:
 			setOrientation(world, x, y, z, ForgeDirection.EAST);
 		}
-    }
-    
-    public static void setOrientation(World world, int x, int y, int z,
+	}
+
+	public static void setOrientation(World world, int x, int y, int z,
 			ForgeDirection direction) {
-		
+
 		int metadata = world.getBlockMetadata(x, y, z) & 0xc;
-		
+
 		switch (direction) {
 		case NORTH:
 			world.setBlockMetadataWithNotify(x, y, z, metadata, 2);
