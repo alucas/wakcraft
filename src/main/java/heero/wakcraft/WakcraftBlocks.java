895eddef30473b0a79d9206f7b743e091b0028bd
Antoine <antoine.lucas.33@gmail.com>
2014-04-15 11:05:13 +0200

    Fix blocks instance names

--- src/main/java/heero/wakcraft/WakcraftBlocks.java
+++ src/main/java/heero/wakcraft/WakcraftBlocks.java
@@ -26,7 +26,9 @@ import cpw.mods.fml.common.registry.GameRegistry;
 
 public class WakcraftBlocks extends Blocks {
 
-	public static Block tannerWorkbench, dragoexpress, phoenix, sufokiaColor, sufokiaSun, sufokiaWave, sufokiaStair, oreLvl1, carpet1, sufokiaGround;
+	public static Block tannerWorkbench, dragoexpress, phoenix, sufokiaColor,
+			sufokiaSun, sufokiaWave, sufokiaStair, ore1, ore2, ore3, ore4,
+			carpet1, sufokiaGround, palisade;
 
 	public static void registerBlocks() {
 		// Basic blocks
@@ -35,12 +37,12 @@ public class WakcraftBlocks extends Blocks {
 		GameRegistry.registerBlock(sufokiaWave = (new BlockSufokiaWave()), ItemBlockSufokiaWave.class, "blockSufokiaWaveBlock");
 		GameRegistry.registerBlock(sufokiaStair = ((new BlockStairs2(sufokiaColor, 0)).setBlockName("SufokiaStair")), "blockSufokiaStair");
 		GameRegistry.registerBlock(sufokiaGround = (new BlockSufokiaGround()), ItemBlockSufokiaGround.class, "sufokiaGroundBlock");
-		GameRegistry.registerBlock(oreLvl1 = (new BlockOre1()), ItemBlockOre1.class, "oreLvl1Block");
-		GameRegistry.registerBlock(oreLvl1 = (new BlockOre2()), ItemBlockOre2.class, "blockOre2");
-		GameRegistry.registerBlock(oreLvl1 = (new BlockOre3()), ItemBlockOre3.class, "blockOre3");
-		GameRegistry.registerBlock(oreLvl1 = (new BlockOre4()), ItemBlockOre4.class, "blockOre4");
+		GameRegistry.registerBlock(ore1 = (new BlockOre1()), ItemBlockOre1.class, "oreLvl1Block");
+		GameRegistry.registerBlock(ore2 = (new BlockOre2()), ItemBlockOre2.class, "blockOre2");
+		GameRegistry.registerBlock(ore3 = (new BlockOre3()), ItemBlockOre3.class, "blockOre3");
+		GameRegistry.registerBlock(ore4 = (new BlockOre4()), ItemBlockOre4.class, "blockOre4");
 		GameRegistry.registerBlock(carpet1 = (new BlockCarpet()), "carpet1Block");
-		GameRegistry.registerBlock(carpet1 = (new BlockPalisade()), "blockPalisade");
+		GameRegistry.registerBlock(palisade = (new BlockPalisade()), "blockPalisade");
 		
 		// Special blocks
 		GameRegistry.registerBlock(tannerWorkbench = (new BlockTannerWorkbench().setHardness(2.5F).setBlockName("tannerWorkbench").setBlockTextureName(WakcraftInfo.MODID.toLowerCase() + ":tannerWorkbench")),	"tannerWorkbench");
