2a6edd12ba78e78ead77376affbb70d987a59b17
Antoine <antoine.lucas.33@gmail.com>
2014-04-16 21:29:43 +0200

    Add RotationUtil class

--- src/main/java/heero/wakcraft/block/BlockPalisade.java
+++ src/main/java/heero/wakcraft/block/BlockPalisade.java
@@ -3,6 +3,7 @@ package heero.wakcraft.block;
 import heero.wakcraft.WakcraftInfo;
 import heero.wakcraft.creativetab.WakcraftCreativeTabs;
 import heero.wakcraft.renderer.RenderBlockPalisade;
+import heero.wakcraft.util.RotationUtil;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.entity.EntityLivingBase;
@@ -97,55 +98,6 @@ public class BlockPalisade extends Block {
 			EntityLivingBase player, ItemStack itemBlock) {
 		super.onBlockPlacedBy(world, x, y, z, player, itemBlock);
 
-		setOrientationFromYaw(world, x, y, z, player.rotationYaw);
-	}
-
-	public static void setOrientationFromYaw(World world, int x, int y, int z,
-			float yaw) {
-		int l = MathHelper.floor_double((double) (yaw * 4.0F / 360.0F) + 0.5D) & 3;
-
-		switch (l) {
-		case 0:
-			setOrientation(world, x, y, z, ForgeDirection.SOUTH);
-			break;
-		case 1:
-			setOrientation(world, x, y, z, ForgeDirection.WEST);
-			break;
-		case 2:
-			setOrientation(world, x, y, z, ForgeDirection.NORTH);
-			break;
-		default:
-			setOrientation(world, x, y, z, ForgeDirection.EAST);
-		}
-	}
-
-	public static void setOrientation(World world, int x, int y, int z,
-			ForgeDirection direction) {
-
-		int metadata = world.getBlockMetadata(x, y, z) & 0xc;
-
-		switch (direction) {
-		case NORTH:
-			world.setBlockMetadataWithNotify(x, y, z, metadata, 2);
-			break;
-		case EAST:
-			world.setBlockMetadataWithNotify(x, y, z, metadata | 0x1, 2);
-			break;
-		case WEST:
-			world.setBlockMetadataWithNotify(x, y, z, metadata | 0x2, 2);
-			break;
-		default:
-			world.setBlockMetadataWithNotify(x, y, z, metadata | 0x3, 2);
-			break;
-		}
-	}
-
-	public static ForgeDirection getOrientationFromMetadata(int metadata) {
-		metadata &= 0x3;
-
-		return metadata == 0x0 ? ForgeDirection.NORTH
-				: metadata == 0x1 ? ForgeDirection.SOUTH
-						: metadata == 0x2 ? ForgeDirection.EAST
-								: ForgeDirection.WEST;
+		RotationUtil.setOrientationFromYaw(world, x, y, z, player.rotationYaw);
 	}
 }
