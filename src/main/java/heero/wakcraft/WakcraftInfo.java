f06bc3bb74bbc126e96f32f3d0b827bb268a0978
Antoine <antoine.lucas.33@gmail.com>
2014-04-21 15:32:59 +0200

    Upgrade 1.7

--- src/main/java/heero/wakcraft/WakcraftInfo.java
+++ src/main/java/heero/wakcraft/WakcraftInfo.java
@@ -2,7 +2,7 @@ package heero.wakcraft;
 
 public class WakcraftInfo {
 	public static final String MODID = "wakcraft";
-	public static final String VERSION = "1.7.2-1.6";
+	public static final String VERSION = "1.7.2-1.7";
 	public static final String READABLE_NAME = "Wakcraft";
 	public static final String PROXY_PATH = "heero.wakcraft.proxy";
 	public static final String PROXY_CLIENT_PATH = PROXY_PATH + ".CombinedClientProxy";
