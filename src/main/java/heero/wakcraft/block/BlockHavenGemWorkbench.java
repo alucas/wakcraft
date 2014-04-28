237f74ec84163c071c548b7a02943d468b8d66f8
Antoine <antoine.lucas.33@gmail.com>
2014-04-28 20:36:29 +0200

    Add haven bag creation

--- src/main/java/heero/wakcraft/block/BlockHavenGemWorkbench.java
+++ src/main/java/heero/wakcraft/block/BlockHavenGemWorkbench.java
@@ -5,14 +5,15 @@ import heero.wakcraft.WakcraftInfo;
 import heero.wakcraft.creativetab.WakcraftCreativeTabs;
 import heero.wakcraft.network.GuiHandler;
 import heero.wakcraft.tileentity.TileEntityHavenGemWorkbench;
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
 
 public class BlockHavenGemWorkbench extends BlockContainer {
-
 	public BlockHavenGemWorkbench() {
 		super(Material.wood);
 		setCreativeTab(WakcraftCreativeTabs.tabSpecialBlock);
@@ -43,5 +44,69 @@ public class BlockHavenGemWorkbench extends BlockContainer {
 	@Override
 	public TileEntity createNewTileEntity(World world, int var2) {
 		return new TileEntityHavenGemWorkbench();
+	}
+
+	@Override
+	public boolean canPlaceBlockAt(World world, int x, int y, int z) {
+		return super.canPlaceBlockAt(world, x, y, z);
+	}
+
+	/**
+     * Called whenever the block is added into the world. Args: world, x, y, z
+     */
+	@Override
+    public void onBlockAdded(World world, int x, int y, int z) {
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				world.setBlock(x - 2 + i, y - 1, z + j, Blocks.stone);
+			}
+		}
+
+		for (int i = 0; i < 2; i++) {
+			world.setBlock(x - 1 + i, y - 1, z + 4, Blocks.stone_slab, 8, 2);
+		}
+
+		for (int i = 0; i < 2; i++) {
+			world.setBlock(x - 4 + i, y - 1, z + 7, Blocks.stone_slab, 8, 2);
+		}
+
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				world.setBlock(x - 2 + i, y - 1, z + 5 + j, Blocks.planks);
+			}
+		}
+
+		fillWalls(world, x - 5, y - 1, z - 1, 4, Blocks.glass_pane, 0);
+
+		super.onBlockAdded(world, x, y, z);
+	}
+
+	@Override
+	public void breakBlock(World world, int x, int y, int z, Block block, int metadata) {
+		fillAir(world, x - 5, y - 1, z - 1, 4);
+
+		super.breakBlock(world, x, y, z, block, metadata);
+	}
+
+	private void fillWalls(World world, int x, int y, int z, int nbLayer, Block block, int metadata) {
+		for (int i = 0; i < 21; i++) {
+		for (int j = 0; j < 24; j++) {
+				if (world.getBlock(x + i, y, z + j) == Blocks.air) {
+					for (int k = 0; k < nbLayer; k++) {
+						world.setBlock(x + i, y + k, z + j, block, metadata, 2);
+					}
+				}
+			}
+		}
+	}
+
+	private void fillAir(World world, int x, int y, int z, int nbLayer) {
+		for (int i = 0; i < 21; i++) {
+			for (int j = 0; j < 24; j++) {
+				for (int k = 0; k < nbLayer; k++) {
+					world.setBlockToAir(x + i, y + k, z + j);
+				}
+			}
+		}
 	}
 }
