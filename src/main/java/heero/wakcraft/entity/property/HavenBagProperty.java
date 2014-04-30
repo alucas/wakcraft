65d2c1208da62630a41ce7dc6c943daee0dff22e
Antoine <antoine.lucas.33@gmail.com>
2014-04-30 16:27:40 +0200

    Fix havenbag properties

--- src/main/java/heero/wakcraft/entity/property/HavenBagProperty.java
+++ src/main/java/heero/wakcraft/entity/property/HavenBagProperty.java
@@ -9,12 +9,12 @@ public class HavenBagProperty implements IExtendedEntityProperties {
 	public static final String IDENTIFIER = "HavenBag";
 
 	private static final String TAG_HAVENBAG = "HavenBag";
-	private static final String TAG_ID = "Id";
+	private static final String TAG_UID = "UID";
 	private static final String TAG_POS_X = "PosX";
 	private static final String TAG_POS_Y = "PosY";
 	private static final String TAG_POS_Z = "PosZ";
 
-	public int id;
+	public int uid;
 	public int posX;
 	public int posY;
 	public int posZ;
@@ -27,10 +27,10 @@ public class HavenBagProperty implements IExtendedEntityProperties {
 	public void saveNBTData(NBTTagCompound tagRoot) {
 		NBTTagCompound tagHavenBag = new NBTTagCompound();
 
-		tagHavenBag.setInteger(TAG_ID, id);
-		tagHavenBag.setInteger(TAG_ID, posX);
-		tagHavenBag.setInteger(TAG_ID, posY);
-		tagHavenBag.setInteger(TAG_ID, posZ);
+		tagHavenBag.setInteger(TAG_UID, uid);
+		tagHavenBag.setInteger(TAG_POS_X, posX);
+		tagHavenBag.setInteger(TAG_POS_Y, posY);
+		tagHavenBag.setInteger(TAG_POS_Z, posZ);
 
 		tagRoot.setTag(TAG_HAVENBAG, tagHavenBag);
 	}
@@ -39,7 +39,7 @@ public class HavenBagProperty implements IExtendedEntityProperties {
 	public void loadNBTData(NBTTagCompound tagRoot) {
 		NBTTagCompound tagHavenBag = tagRoot.getCompoundTag(TAG_HAVENBAG);
 
-		id = tagHavenBag.getInteger(TAG_ID);
+		uid = tagHavenBag.getInteger(TAG_UID);
 		posX = tagHavenBag.getInteger(TAG_POS_X);
 		posY = tagHavenBag.getInteger(TAG_POS_Y);
 		posZ = tagHavenBag.getInteger(TAG_POS_Z);
