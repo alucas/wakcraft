63bc748a01d4d7c723221d4f2785864406a64e2a
Antoine <antoine.lucas.33@gmail.com>
2014-04-30 15:21:51 +0200

    Add some information to havenbag  properties

--- src/main/java/heero/wakcraft/entity/property/HavenBagProperty.java
+++ src/main/java/heero/wakcraft/entity/property/HavenBagProperty.java
@@ -8,21 +8,40 @@ import net.minecraftforge.common.IExtendedEntityProperties;
 public class HavenBagProperty implements IExtendedEntityProperties {
 	public static final String IDENTIFIER = "HavenBag";
 
+	private static final String TAG_HAVENBAG = "HavenBag";
 	private static final String TAG_ID = "Id";
+	private static final String TAG_POS_X = "PosX";
+	private static final String TAG_POS_Y = "PosY";
+	private static final String TAG_POS_Z = "PosZ";
 
 	public int id;
+	public int posX;
+	public int posY;
+	public int posZ;
 
 	@Override
 	public void init(Entity entity, World world) {
 	}
 
 	@Override
-	public void saveNBTData(NBTTagCompound compound) {
-		compound.setInteger(TAG_ID, id);
+	public void saveNBTData(NBTTagCompound tagRoot) {
+		NBTTagCompound tagHavenBag = new NBTTagCompound();
+
+		tagHavenBag.setInteger(TAG_ID, id);
+		tagHavenBag.setInteger(TAG_ID, posX);
+		tagHavenBag.setInteger(TAG_ID, posY);
+		tagHavenBag.setInteger(TAG_ID, posZ);
+
+		tagRoot.setTag(TAG_HAVENBAG, tagHavenBag);
 	}
 
 	@Override
-	public void loadNBTData(NBTTagCompound compound) {
-		id = compound.getInteger(TAG_ID);
+	public void loadNBTData(NBTTagCompound tagRoot) {
+		NBTTagCompound tagHavenBag = tagRoot.getCompoundTag(TAG_HAVENBAG);
+
+		id = tagHavenBag.getInteger(TAG_ID);
+		posX = tagHavenBag.getInteger(TAG_POS_X);
+		posY = tagHavenBag.getInteger(TAG_POS_Y);
+		posZ = tagHavenBag.getInteger(TAG_POS_Z);
 	}
 }
