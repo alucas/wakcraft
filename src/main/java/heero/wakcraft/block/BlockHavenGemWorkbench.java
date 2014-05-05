c2612e863a590c5bc4288fd8e5107bc6103fe111
Antoine <antoine.lucas.33@gmail.com>
2014-05-05 11:36:57 +0200

    Update haven gem workbench textures

--- src/main/java/heero/wakcraft/block/BlockHavenGemWorkbench.java
+++ src/main/java/heero/wakcraft/block/BlockHavenGemWorkbench.java
@@ -1,27 +1,30 @@
 package heero.wakcraft.block;
 
 import heero.wakcraft.Wakcraft;
-import heero.wakcraft.WakcraftBlocks;
 import heero.wakcraft.WakcraftInfo;
 import heero.wakcraft.creativetab.WakcraftCreativeTabs;
 import heero.wakcraft.network.GuiHandler;
 import heero.wakcraft.tileentity.TileEntityHavenGemWorkbench;
-import net.minecraft.block.Block;
 import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.init.Blocks;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ChatComponentText;
+import net.minecraft.util.IIcon;
 import net.minecraft.world.World;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 
 public class BlockHavenGemWorkbench extends BlockContainer {
+	private IIcon iconSide;
+	private IIcon iconTop;
+
 	public BlockHavenGemWorkbench() {
 		super(Material.wood);
 		setCreativeTab(WakcraftCreativeTabs.tabSpecialBlock);
 		setBlockName("HavenGemWorkbench");
-		setBlockTextureName(WakcraftInfo.MODID.toLowerCase() + ":havengemworkbench");
 	}
 
 	public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer player, int side, float hitX, float hitY, float hitZ) {
@@ -56,5 +59,25 @@ public class BlockHavenGemWorkbench extends BlockContainer {
 		}
 
 		return false;
+	}
+
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void registerBlockIcons(IIconRegister registerer) {
+		iconSide = registerer.registerIcon(WakcraftInfo.MODID.toLowerCase() + ":havengemworkbench");
+		iconTop = registerer.registerIcon(WakcraftInfo.MODID.toLowerCase() + ":havengemworkbench_top");
+	}
+
+	/**
+	 * Gets the block's texture. Args: side, meta
+	 */
+	@Override
+	@SideOnly(Side.CLIENT)
+	public IIcon getIcon(int side, int metadata) {
+		if (side == 1) {
+			return iconTop;
+		}
+
+		return iconSide;
 	}
 }
