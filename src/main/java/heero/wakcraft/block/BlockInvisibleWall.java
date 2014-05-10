f7699a0483c6f5751d8fe2595a8d30a361230cb2
Antoine <antoine.lucas.33@gmail.com>
2014-05-10 15:26:10 +0200

    Update the havenbag world render

--- src/main/java/heero/wakcraft/block/BlockInvisibleWall.java
+++ src/main/java/heero/wakcraft/block/BlockInvisibleWall.java
@@ -1,8 +1,11 @@
 package heero.wakcraft.block;
 
+import heero.wakcraft.WakcraftConfig;
 import heero.wakcraft.creativetab.WakcraftCreativeTabs;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
 
 public class BlockInvisibleWall extends Block {
 
@@ -26,5 +29,14 @@ public class BlockInvisibleWall extends Block {
 	@Override
 	public int getRenderType() {
 		return -1;
+	}
+
+	@Override
+	public int getLightValue(IBlockAccess world, int x, int y, int z) {
+		if (world instanceof World && ((World) world).provider.dimensionId == WakcraftConfig.havenBagDimensionId) {
+			return 15;
+		}
+
+		return super.getLightValue(world, x, y, z);
 	}
 }
--- src/main/java/heero/wakcraft/world/WorldProviderHaveBag.java
+++ src/main/java/heero/wakcraft/world/WorldProviderHaveBag.java
@@ -1,7 +1,12 @@
 package heero.wakcraft.world;
 
 import heero.wakcraft.WakcraftConfig;
+import heero.wakcraft.renderer.world.EndSkyRenderer;
+import net.minecraft.util.MathHelper;
+import net.minecraft.util.Vec3;
 import net.minecraft.world.WorldProvider;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 
 public class WorldProviderHaveBag extends WorldProvider {
 	public static final String NAME = "WakcraftHavenBag";
@@ -13,7 +18,9 @@ public class WorldProviderHaveBag extends WorldProvider {
 	public void registerWorldChunkManager() {
 		super.registerWorldChunkManager();
 
+		this.hasNoSky = true;
 		this.dimensionId = WakcraftConfig.havenBagDimensionId;
+		this.setSkyRenderer(new EndSkyRenderer());
 	}
 
 	/**
@@ -22,5 +29,99 @@ public class WorldProviderHaveBag extends WorldProvider {
 	@Override
 	public String getDimensionName() {
 		return NAME;
+	}
+
+	/**
+	 * A message to display to the user when they transfer to this dimension.
+	 *
+	 * @return The message to be displayed
+	 */
+	public String getWelcomeMessage() {
+		return "Entering your HavenBag";
+	}
+
+	/**
+	 * A Message to display to the user when they transfer out of this
+	 * dismension.
+	 *
+	 * @return The message to be displayed
+	 */
+	public String getDepartMessage() {
+		return "Leaving your HavenBag";
+	}
+
+	/**
+	 * Returns 'true' if in the "main surface world", but 'false' if in the
+	 * Nether or End dimensions.
+	 */
+	public boolean isSurfaceWorld() {
+		return false;
+	}
+
+	/**
+	 * Calculates the angle of sun and moon in the sky relative to a specified
+	 * time (usually worldTime)
+	 */
+	public float calculateCelestialAngle(long par1, float par3) {
+		return 0.0F;
+	}
+
+	/**
+	 * Returns array with sunrise/sunset colors
+	 */
+	@SideOnly(Side.CLIENT)
+	public float[] calcSunriseSunsetColors(float par1, float par2) {
+		return null;
+	}
+
+	@SideOnly(Side.CLIENT)
+	public boolean isSkyColored() {
+		return false;
+	}
+
+	/**
+	 * True if the player can respawn in this dimension (true = overworld, false
+	 * = nether).
+	 */
+	public boolean canRespawnHere() {
+		return false;
+	}
+
+	public int getAverageGroundLevel() {
+		return 10;
+	}
+
+	/**
+	 * Returns true if the given X,Z coordinate should show environmental fog.
+	 */
+	@SideOnly(Side.CLIENT)
+	public boolean doesXZShowFog(int x, int z) {
+		return true;
+	}
+
+	/**
+	 * Return Vec3D with biome specific fog color
+	 */
+	@SideOnly(Side.CLIENT)
+	public Vec3 getFogColor(float par1, float par2) {
+		int i = 10518688;
+		float f2 = MathHelper.cos(par1 * (float) Math.PI * 2.0F) * 2.0F + 0.5F;
+
+		if (f2 < 0.0F) {
+			f2 = 0.0F;
+		}
+
+		if (f2 > 1.0F) {
+			f2 = 1.0F;
+		}
+
+		float f3 = (float) (i >> 16 & 255) / 255.0F;
+		float f4 = (float) (i >> 8 & 255) / 255.0F;
+		float f5 = (float) (i & 255) / 255.0F;
+		f3 *= f2 * 0.0F + 0.15F;
+		f4 *= f2 * 0.0F + 0.15F;
+		f5 *= f2 * 0.0F + 0.15F;
+		return this.worldObj.getWorldVec3Pool().getVecFromPool((double) f3,
+				(double) f4, (double) f5);
 	}
 }
