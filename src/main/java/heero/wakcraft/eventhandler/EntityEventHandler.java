31400be74a0cd9eb4fec61f0c1f52e0de5a01f48
Antoine <antoine.lucas.33@gmail.com>
2014-05-20 16:22:06 +0200

    Fix add FightProperty

--- src/main/java/heero/wakcraft/eventhandler/EntityEventHandler.java
+++ src/main/java/heero/wakcraft/eventhandler/EntityEventHandler.java
@@ -3,6 +3,7 @@ package heero.wakcraft.eventhandler;
 import heero.wakcraft.entity.property.FightProperty;
 import heero.wakcraft.entity.property.HavenBagProperty;
 import heero.wakcraft.entity.property.ProfessionProperty;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraftforge.event.entity.EntityEvent.EntityConstructing;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
@@ -10,7 +11,9 @@ import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 public class EntityEventHandler {
 	@SubscribeEvent
 	public void onEntityConstructing(EntityConstructing event) {
-		event.entity.registerExtendedProperties(FightProperty.IDENTIFIER, new FightProperty());
+		if (event.entity instanceof EntityLivingBase) {
+			event.entity.registerExtendedProperties(FightProperty.IDENTIFIER, new FightProperty());
+		}
 
 		if (event.entity instanceof EntityPlayer) {
 			EntityPlayer player = (EntityPlayer) event.entity;
