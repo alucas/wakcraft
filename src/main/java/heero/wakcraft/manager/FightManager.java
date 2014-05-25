cd147b10837b793bbd0097c7cec268a8eec0deca
Antoine <antoine.lucas.33@gmail.com>
2014-05-25 16:30:22 +0200

    Generate fight map

--- src/main/java/heero/wakcraft/manager/FightManager.java
+++ src/main/java/heero/wakcraft/manager/FightManager.java
@@ -1,5 +1,6 @@
 package heero.wakcraft.manager;
 
+import heero.wakcraft.WBlocks;
 import heero.wakcraft.Wakcraft;
 import heero.wakcraft.entity.property.FightProperty;
 import heero.wakcraft.event.FightEvent;
@@ -15,6 +16,8 @@ import java.util.Map;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.init.Blocks;
+import net.minecraft.util.ChunkCoordinates;
 import net.minecraft.world.World;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.living.LivingDeathEvent;
@@ -23,7 +26,10 @@ import cpw.mods.fml.common.FMLLog;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 
 public class FightManager {
+	// Map of <FightId, Teams<Entities<Id>>>
 	protected Map<Integer, List<List<Integer>>> fights = new HashMap<Integer, List<List<Integer>>>();
+	// Map of <FightId, mapBlocks<blockPos>>
+	protected Map<Integer, List<ChunkCoordinates>> maps = new HashMap<Integer, List<ChunkCoordinates>>();
 
 	@SubscribeEvent
 	public void onAttackEntityEvent(AttackEntityEvent event) {
@@ -55,7 +61,12 @@ public class FightManager {
 		}
 
 		if (!properties.isFighting() && !targetProperties.isFighting()) {
-			initFight((EntityPlayerMP) event.entityPlayer, target);
+			int fightId = initFight((EntityPlayerMP) event.entityPlayer, target);
+			if (fightId == -1) {
+				return;
+			}
+
+			createFightMap(fightId, (EntityPlayerMP) event.entityPlayer);
 
 			event.setCanceled(true);
 			return;
@@ -88,17 +99,17 @@ public class FightManager {
 		updateFight(event.entity.worldObj, fightId);
 	}
 
-	protected void initFight(EntityPlayerMP assailant, EntityLivingBase target) {
+	protected int initFight(EntityPlayerMP assailant, EntityLivingBase target) {
 		FightProperty assailantProperties = (FightProperty) assailant.getExtendedProperties(FightProperty.IDENTIFIER);
 		if (assailantProperties == null) {
 			FMLLog.warning("Error while loading the Fight properties of player : %s", assailant.getDisplayName());
-			return;
+			return -1;
 		}
 
 		FightProperty targetProperties = (FightProperty) target.getExtendedProperties(FightProperty.IDENTIFIER);
 		if (targetProperties == null) {
 			FMLLog.warning("Error while loading the Fight properties of : %s", target.getClass().getName());
-			return;
+			return -1;
 		}
 
 		int fightId = assailant.worldObj.getUniqueDataId("fightId");
@@ -117,6 +128,52 @@ public class FightManager {
 
 		if (target instanceof EntityPlayerMP) {
 			Wakcraft.packetPipeline.sendTo(new PacketFight(Type.START, fightId, fighters), (EntityPlayerMP) target);
+		}
+
+		return fightId;
+	}
+
+	protected void createFightMap(int fightId, EntityPlayerMP player) {
+		int posX = (int) Math.floor(player.posX);
+		int posY = (int) Math.floor(player.posY - 1.61);
+		int posZ = (int) Math.floor(player.posZ);
+
+		List<ChunkCoordinates> blockList = new ArrayList<ChunkCoordinates>();
+
+		int radius = 5;
+		for (int j = 0; j <= radius * 2; j++) {
+			setFightWall(blockList, player.worldObj, posX + radius - j, posY, posZ + radius + 1);
+		}
+		for (int j = 0; j <= radius * 2; j++) {
+			setFightWall(blockList, player.worldObj, posX + radius - j, posY, posZ - radius - 1);
+		}
+		for (int j = 0; j <= radius * 2; j++) {
+			setFightWall(blockList, player.worldObj, posX + radius + 1, posY, posZ + radius - j);
+		}
+		for (int j = 0; j <= radius * 2; j++) {
+			setFightWall(blockList, player.worldObj, posX - radius - 1, posY, posZ + radius - j);
+		}
+
+		maps.put(fightId, blockList);
+	}
+
+	protected void setFightWall(List<ChunkCoordinates> blockList, World world, int x, int y, int z) {
+		for (; y < world.getHeight() && !world.getBlock(x, y, z).equals(Blocks.air); ++y);
+		for (; y >= 0 && world.getBlock(x, y, z).equals(Blocks.air); --y);
+		world.setBlock(x, y + 1, z, WBlocks.fightMovement);
+		blockList.add(new ChunkCoordinates(x, y + 1, z));
+
+		if(world.getBlock(x, y + 2, z).equals(Blocks.air)) {
+			world.setBlock(x, y + 2, z, WBlocks.fightMovement);
+			blockList.add(new ChunkCoordinates(x, y + 2, z));
+		}
+	}
+
+	protected void removeFightMap(World world, int fightId) {
+		List<ChunkCoordinates> blockList = maps.remove(fightId);
+
+		for (ChunkCoordinates block : blockList) {
+			world.setBlockToAir(block.posX, block.posY, block.posZ);
 		}
 	}
 
@@ -147,6 +204,8 @@ public class FightManager {
 
 			if (!living) {
 				stopFight(world, fightId);
+				removeFightMap(world, fightId);
+
 				return;
 			}
 		}
@@ -182,55 +241,63 @@ public class FightManager {
 	public void onFightEvent(FightEvent event) {
 		switch (event.type) {
 		case START:
-			Iterator<List<Integer>> teams = event.fighters.iterator();
-			while (teams.hasNext()) {
-				Iterator<Integer> entities = teams.next().iterator();
-				while (entities.hasNext()) {
-					Entity entity = event.world.getEntityByID(entities.next());
-					if (entity == null || !(entity instanceof EntityLivingBase)) {
-						FMLLog.warning("Wrond fighting entity id");
-						return;
-					}
-
-					FightProperty entityProperties = (FightProperty) entity.getExtendedProperties(FightProperty.IDENTIFIER);
-					if (entityProperties == null) {
-						FMLLog.warning("Error while loading the Fight properties of player : %s", entity.getClass().getName());
-						return;
-					}
-
-					entityProperties.setFightId(event.fightId);
-				}
-			}
+			addFightersToFight(event.world, event.fighters, event.fightId);
 
 			fights.put(event.fightId, event.fighters);
 			break;
 
 		case STOP:
-			teams = event.fighters.iterator();
-			while (teams.hasNext()) {
-				Iterator<Integer> entities = teams.next().iterator();
-				while (entities.hasNext()) {
-					Entity entity = event.world.getEntityByID(entities.next());
-					if (entity == null || !(entity instanceof EntityLivingBase)) {
-						FMLLog.warning("Wrond fighting entity id");
-						return;
-					}
-
-					FightProperty entityProperties = (FightProperty) entity.getExtendedProperties(FightProperty.IDENTIFIER);
-					if (entityProperties == null) {
-						FMLLog.warning("Error while loading the Fight properties of player : %s", entity.getClass().getName());
-						return;
-					}
-
-					entityProperties.resetFightId();
-				}
-			}
+			removeFightersFromFight(event.world, event.fighters);
 
 			fights.remove(event.fightId);
 			break;
 
 		default:
 			break;
+		}
+	}
+
+	protected void addFightersToFight(World world, List<List<Integer>> fighters, int fightId) {
+		Iterator<List<Integer>> teams = fighters.iterator();
+		while (teams.hasNext()) {
+			Iterator<Integer> entities = teams.next().iterator();
+			while (entities.hasNext()) {
+				Entity entity = world.getEntityByID(entities.next());
+				if (entity == null || !(entity instanceof EntityLivingBase)) {
+					FMLLog.warning("Wrond fighting entity id");
+					return;
+				}
+
+				FightProperty entityProperties = (FightProperty) entity.getExtendedProperties(FightProperty.IDENTIFIER);
+				if (entityProperties == null) {
+					FMLLog.warning("Error while loading the Fight properties of player : %s", entity.getClass().getName());
+					return;
+				}
+
+				entityProperties.setFightId(fightId);
+			}
+		}
+	}
+
+	protected void removeFightersFromFight(World world, List<List<Integer>> fighters) {
+		Iterator<List<Integer>> teams = fighters.iterator();
+		while (teams.hasNext()) {
+			Iterator<Integer> entities = teams.next().iterator();
+			while (entities.hasNext()) {
+				Entity entity = world.getEntityByID(entities.next());
+				if (entity == null || !(entity instanceof EntityLivingBase)) {
+					FMLLog.warning("Wrond fighting entity id");
+					return;
+				}
+
+				FightProperty entityProperties = (FightProperty) entity.getExtendedProperties(FightProperty.IDENTIFIER);
+				if (entityProperties == null) {
+					FMLLog.warning("Error while loading the Fight properties of player : %s", entity.getClass().getName());
+					return;
+				}
+
+				entityProperties.resetFightId();
+			}
 		}
 	}
 }
