7f15291b2ee02aaf6d60e0855798bb11deddd9b1
Antoine <antoine.lucas.33@gmail.com>
2014-05-26 19:56:48 +0200

    Fix proxies path

--- src/main/java/heero/mc/mod/wakcraft/WInfo.java
+++ src/main/java/heero/mc/mod/wakcraft/WInfo.java
@@ -3,7 +3,7 @@ package heero.mc.mod.wakcraft;
 public class WInfo {
 	public static final String MODID = "wakcraft";
 	public static final String READABLE_NAME = "Wakcraft";
-	public static final String PROXY_PATH = "heero.wakcraft.proxy";
+	public static final String PROXY_PATH = "heero.mc.mod.wakcraft.proxy";
 	public static final String PROXY_CLIENT_PATH = PROXY_PATH + ".CombinedClientProxy";
 	public static final String PROXY_SERVER_PATH = PROXY_PATH + ".DedicatedServerProxy";
 }
