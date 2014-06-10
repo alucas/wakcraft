86a901595a8c7a1dc19d35a3ebadd9b8ee5a5537
Antoine <antoine.lucas.33@gmail.com>
2014-06-11 01:54:42 +0200

    Merge constructors

--- src/main/java/heero/mc/mod/wakcraft/fight/FightInfo.java
+++ src/main/java/heero/mc/mod/wakcraft/fight/FightInfo.java
@@ -6,19 +6,16 @@ import java.util.Set;
 
 import net.minecraft.entity.EntityLivingBase;
 
+import com.sun.istack.internal.Nullable;
+
 public class FightInfo {
 	protected List<List<EntityLivingBase>> fighters;
 	protected Set<FightBlockCoordinates> fightBlocks;
 	protected List<FightBlockCoordinates> startBlocks;
 
-	public FightInfo(List<List<EntityLivingBase>> fighters, Set<FightBlockCoordinates> fightBlocks, List<FightBlockCoordinates> startBlocks) {
+	public FightInfo(List<List<EntityLivingBase>> fighters, @Nullable Set<FightBlockCoordinates> fightBlocks, List<FightBlockCoordinates> startBlocks) {
 		this.fighters = fighters;
 		this.fightBlocks = fightBlocks;
-		this.startBlocks = startBlocks;
-	}
-
-	public FightInfo(List<List<EntityLivingBase>> fighters, List<FightBlockCoordinates> startBlocks) {
-		this.fighters = fighters;
 		this.startBlocks = startBlocks;
 	}
 
