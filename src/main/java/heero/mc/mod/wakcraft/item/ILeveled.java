ee7f57ad1407825fbc68da05e20752dcfb9a5d78
Antoine <antoine.lucas.33@gmail.com>
2014-07-10 00:37:07 +0200

    Add ILeveled interface

---
+++ src/main/java/heero/mc/mod/wakcraft/item/ILeveled.java
@@ -0,0 +1,5 @@
+package heero.mc.mod.wakcraft.item;
+
+public interface ILeveled {
+	public int getLevel(final int metadata);
+}
--- src/main/java/heero/mc/mod/wakcraft/item/ItemWithLevel.java
+++ src/main/java/heero/mc/mod/wakcraft/item/ItemWithLevel.java
@@ -2,7 +2,7 @@ package heero.mc.mod.wakcraft.item;
 
 import net.minecraft.item.Item;
 
-public class ItemWithLevel extends Item {
+public class ItemWithLevel extends Item implements ILeveled {
 	protected int level;
 
 	public ItemWithLevel(int level) {
