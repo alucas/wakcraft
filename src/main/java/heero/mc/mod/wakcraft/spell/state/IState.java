11f1c113906bbe2d98645b93dcad983458705273
Antoine <antoine.lucas.33@gmail.com>
2014-07-16 16:45:24 +0200

    Add IState interface

--- src/main/java/heero/mc/mod/wakcraft/spell/state/IState.java
+++ src/main/java/heero/mc/mod/wakcraft/spell/state/IState.java
@@ -1,5 +1,43 @@
 package heero.mc.mod.wakcraft.spell.state;
 
-public interface IState {
+import heero.mc.mod.wakcraft.spell.ISpell;
+import heero.mc.mod.wakcraft.spell.effect.IEffect;
 
+import java.util.List;
+
+/**
+ * Main state interface.
+ */
+public interface IState {
+	/**
+	 * Returns the name of the state.
+	 * @return	The name of the state.
+	 */
+	public String getName();
+
+	/**
+	 * Returns the description of the state.
+	 * @return	The description of the state.
+	 */
+	public String getDescription();
+
+	/**
+	 * Returns the level of the state.
+	 * @param metadata	Value used to retrieve the state's level.
+	 * @return	The level of the state.
+	 */
+	public int getLevel(final int metadata);
+
+	/**
+	 * Add an effect to the state.
+	 * @param effect	The effect to add to the state.
+	 * @return	This instance, for usage commodity.
+	 */
+	public ISpell setEffect(final IEffect effect);
+
+	/**
+	 * Returns the list of effects of this state.
+	 * @return	The list of effects of this state.
+	 */
+	public List<IEffect> getEffects();
 }
