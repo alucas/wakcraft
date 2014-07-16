ba0b129d705ac322842fc31b65dcb5dd43c9898a
Antoine <antoine.lucas.33@gmail.com>
2014-07-16 16:27:17 +0200

    Add Effect State interface

--- src/main/java/heero/mc/mod/wakcraft/spell/effect/IEffectState.java
+++ src/main/java/heero/mc/mod/wakcraft/spell/effect/IEffectState.java
@@ -1,5 +1,26 @@
 package heero.mc.mod.wakcraft.spell.effect;
 
-public interface IEffectState {
+import heero.mc.mod.wakcraft.spell.state.IState;
 
+/**
+ * State application effect.
+ */
+public interface IEffectState extends IEffect {
+	/**
+	 * Returns the state to apply.
+	 * @return	The state to apply.
+	 */
+	public IState getState();
+
+	/**
+	 * Returns the level of the state to apply or add.
+	 * @return	The level of the state to apply or add.
+	 */
+	public int getLevel();
+
+	/**
+	 * Returns the probability to apply the state.
+	 * @return	The probability to apply the state.
+	 */
+	public int getProbability();
 }
