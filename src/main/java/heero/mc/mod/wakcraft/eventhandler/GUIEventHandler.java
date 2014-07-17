7753c2ddd5a98c1886d4596ca37460f6539f8f1b
Antoine <antoine.lucas.33@gmail.com>
2014-07-17 02:19:52 +0200

    Add color to tooltip

--- src/main/java/heero/mc/mod/wakcraft/eventhandler/GUIEventHandler.java
+++ src/main/java/heero/mc/mod/wakcraft/eventhandler/GUIEventHandler.java
@@ -11,6 +11,7 @@ import heero.mc.mod.wakcraft.spell.effect.IEffect;
 import heero.mc.mod.wakcraft.util.StringUtil;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.inventory.GuiInventory;
+import net.minecraft.util.EnumChatFormatting;
 import net.minecraft.util.StatCollector;
 import net.minecraftforge.client.event.GuiOpenEvent;
 import net.minecraftforge.event.entity.player.ItemTooltipEvent;
@@ -32,6 +33,9 @@ public class GUIEventHandler {
 		}
 	}
 
+	private static final String stringEffects = StatCollector.translateToLocal("effect.category.effects");
+	private static final String stringCriticalEffects = StatCollector.translateToLocal("effect.category.effectsCritical");
+	private static final String stringConditions = StatCollector.translateToLocal("effect.category.conditions");
 	@SubscribeEvent
 	public void onItemTooltipEvent(ItemTooltipEvent event) {
 		if (event.itemStack.getItem() instanceof ISpell) {
@@ -39,28 +43,37 @@ public class GUIEventHandler {
 			final int spellLevel = spell.getLevel(event.itemStack.getItemDamage());
 
 			if (spell instanceof IPassiveSpell) {
-				event.toolTip.add("Effets :");
+				event.toolTip.add(EnumChatFormatting.BLUE + stringEffects);
 
 				for (IEffect effect : ((IPassiveSpell) spell).getEffects()) {
 					event.toolTip.add(effect.getDescription(spellLevel));
 				}
+
+				event.toolTip.add("");
 			} else if (spell instanceof IActiveSpell) {
-				event.toolTip.add("Effets :");
+				event.toolTip.add(EnumChatFormatting.BLUE + stringEffects);
 
 				for (IEffect effect : ((IActiveSpell) spell).getEffects()) {
 					event.toolTip.add(effect.getDescription(spellLevel));
 				}
 
-				event.toolTip.add("Critical effets :");
+				event.toolTip.add("");
+				event.toolTip.add(EnumChatFormatting.BLUE + stringCriticalEffects);
 
 				for (IEffect effect : ((IActiveSpell) spell).getEffectsCritical()) {
 					event.toolTip.add(effect.getDescription(spellLevel));
 				}
 
-				event.toolTip.add("Usage conditions :");
+				event.toolTip.add("");
 
-				for (ICondition condition : ((IActiveSpell) spell).getConditions()) {
-					event.toolTip.add(condition.getDescription(spellLevel));
+				if (((IActiveSpell) spell).getConditions().size() > 0) {
+					event.toolTip.add(EnumChatFormatting.BLUE + stringConditions);
+
+					for (ICondition condition : ((IActiveSpell) spell).getConditions()) {
+						event.toolTip.add(condition.getDescription(spellLevel));
+					}
+
+					event.toolTip.add("");
 				}
 			}
 
