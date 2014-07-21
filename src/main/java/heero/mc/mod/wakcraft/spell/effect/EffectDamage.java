79ee1a674fc69bd00f2da34f9cc083530f9b950a
Antoine <antoine.lucas.33@gmail.com>
2014-07-21 22:46:23 +0200

    Refactor EffectDamage

--- src/main/java/heero/mc/mod/wakcraft/spell/effect/EffectDamage.java
+++ src/main/java/heero/mc/mod/wakcraft/spell/effect/EffectDamage.java
@@ -3,30 +3,30 @@ package heero.mc.mod.wakcraft.spell.effect;
 import heero.mc.mod.wakcraft.characteristic.Characteristic;
 
 /**
- * Main IEffectDamage implementation.
+ * Elemental spells' IEffectDamage implementation.
  */
 public class EffectDamage extends EffectCharacteristic implements IEffectDamage {
 	private final IEffectElement element;
 
 	/**
 	 * Main constructor.
-	 * @param damageBase	The damage base value.
-	 * @param damageFactor	The damage factor value.
+	 * @param damageMin	The minimal damage value.
+	 * @param damageMax	The maximal damage value.
 	 * @param effectArea	The area of the effect.
 	 */
-	public EffectDamage(final int damageBase, final float damageFactor, IEffectElement element, IEffectArea effectArea) {
-		super(Characteristic.HEALTH, -damageBase, -damageFactor, 1, 0);
+	public EffectDamage(final int damageMin, final int damageMax, IEffectElement element, IEffectArea effectArea) {
+		super(Characteristic.HEALTH, -damageMin, - ((damageMax - damageMin) / 200.0F), 1, 0);
 
 		this.element = element;
 	}
 
 	/**
 	 * Constructor with default value of effect's area : POINT
-	 * @param damageBase	The damage vase value.
-	 * @param damageFactor	The damage factor value.
+	 * @param damageMin	The minimal damage value.
+	 * @param damageMax	The maximal damage value.
 	 */
-	public EffectDamage(final int damageBase, final float damageFactor, IEffectElement element) {
-		this(damageBase, damageFactor, element, EffectArea.POINT);
+	public EffectDamage(final int damageMin, final int damageMax, IEffectElement element) {
+		this(damageMin, damageMax, element, EffectArea.POINT);
 	}
 
 	@Override
