ebd793d8733b4e1d2db96629ef12998137dac0c1
Antoine <antoine.lucas.33@gmail.com>
2014-07-25 16:52:35 +0200

    Add spell casting tests

--- src/main/java/heero/mc/mod/wakcraft/fight/FightUtil.java
+++ src/main/java/heero/mc/mod/wakcraft/fight/FightUtil.java
@@ -1,14 +1,35 @@
 package heero.mc.mod.wakcraft.fight;
 
+import heero.mc.mod.wakcraft.characteristic.Characteristic;
+import heero.mc.mod.wakcraft.helper.FightHelper;
 import heero.mc.mod.wakcraft.spell.IActiveSpell;
 import heero.mc.mod.wakcraft.spell.IRangeMode;
 import heero.mc.mod.wakcraft.spell.RangeMode;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ChunkCoordinates;
 import net.minecraft.util.MathHelper;
 import net.minecraft.util.MovingObjectPosition;
 
 public class FightUtil {
+	public static boolean canCastSpell(final EntityPlayer fighter, final ChunkCoordinates targetPosition) {
+		ChunkCoordinates fighterPosition = FightHelper.getCurrentPosition(fighter);
+		ItemStack spellStack = FightHelper.getCurrentSpell(fighter);
+		if (!FightUtil.isAimingPositionValid(fighterPosition, targetPosition, spellStack)) {
+			return false;
+		}
+
+		if (!FightUtil.isSpellCostAvailable(fighter, spellStack)) {
+			return false;
+		}
+
+		if (!FightUtil.isSpellConditionValid(fighter, spellStack)) {
+			return false;
+		}
+
+		return true;
+	}
+
 	public static boolean isAimingPositionValid(final ChunkCoordinates fighterPosition, final MovingObjectPosition target, final ItemStack spellStack) {
 		return isAimingPositionValid(fighterPosition, target.blockX, target.blockY, target.blockZ, spellStack);
 	}
@@ -58,5 +79,70 @@ public class FightUtil {
 		}
 
 		return true;
+	}
+
+	public static boolean isSpellCostAvailable(final EntityPlayer fighter, final ItemStack spellStack) {
+		int wakfuPoint = FightHelper.getFightCharacteristic(fighter, Characteristic.WAKFU);
+		int movementPoint = FightHelper.getFightCharacteristic(fighter, Characteristic.MOVEMENT);
+		int actionPoint = FightHelper.getFightCharacteristic(fighter, Characteristic.ACTION);
+
+		int wakfuCost = getSpellWakfuCost(spellStack);
+		int movementCost = getSpellMovementCost(spellStack);
+		int actionCost = getSpellActionCost(spellStack);
+
+		if (wakfuCost > wakfuPoint || movementCost > movementPoint || actionCost > actionPoint) {
+			return false;
+		}
+
+		return true;
+	}
+
+	public static int getSpellWakfuCost(final ItemStack spellStack) {
+		if (spellStack == null || spellStack.getItem() == null) {
+			return 0;
+		}
+
+		if (spellStack.getItem() instanceof IActiveSpell) {
+			return ((IActiveSpell) spellStack.getItem()).getWakfuCost();
+		}
+
+		return 0;
+	}
+
+	public static int getSpellMovementCost(final ItemStack spellStack) {
+		if (spellStack == null || spellStack.getItem() == null) {
+			return 0;
+		}
+
+		if (spellStack.getItem() instanceof IActiveSpell) {
+			return ((IActiveSpell) spellStack.getItem()).getMovementCost();
+		}
+
+		return 0;
+	}
+
+	public static int getSpellActionCost(final ItemStack spellStack) {
+		if (spellStack == null || spellStack.getItem() == null) {
+			return 5;
+		}
+
+		if (spellStack.getItem() instanceof IActiveSpell) {
+			return ((IActiveSpell) spellStack.getItem()).getActionCost();
+		}
+
+		return 5;
+	}
+
+	public static boolean isSpellConditionValid(final EntityPlayer fighter, final ItemStack spellStack) {
+		// TODO : Test spell conditions
+		return true;
+	}
+
+	public static void tryCastSpell(final EntityPlayer fighter, final ChunkCoordinates targetPosition) {
+		if (!canCastSpell(fighter, targetPosition)) {
+			return;
+		}
+
+		// TODO : Cast spell
 	}
 }
--- src/main/java/heero/mc/mod/wakcraft/network/handler/fight/HandlerServerFightCastSpell.java
+++ src/main/java/heero/mc/mod/wakcraft/network/handler/fight/HandlerServerFightCastSpell.java
@@ -1,11 +1,10 @@
 package heero.mc.mod.wakcraft.network.handler.fight;
 
+import heero.mc.mod.wakcraft.fight.FightManager;
 import heero.mc.mod.wakcraft.fight.FightUtil;
 import heero.mc.mod.wakcraft.helper.FightHelper;
 import heero.mc.mod.wakcraft.network.packet.fight.PacketFightCastSpell;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.item.ItemStack;
-import net.minecraft.util.ChunkCoordinates;
 import cpw.mods.fml.common.network.simpleimpl.IMessage;
 import cpw.mods.fml.common.network.simpleimpl.IMessageHandler;
 import cpw.mods.fml.common.network.simpleimpl.MessageContext;
@@ -23,11 +22,11 @@ public class HandlerServerFightCastSpell implements IMessageHandler<PacketFightC
 			throw new RuntimeException("The entity " + player + " is not fighting");
 		}
 
-		ChunkCoordinates fighterPosition = FightHelper.getCurrentPosition(player);
-		ItemStack spellStack = FightHelper.getCurrentSpell(player);
-		if (!FightUtil.isAimingPositionValid(fighterPosition, message.targetPosition, spellStack)) {
-			throw new RuntimeException("Invalid position to cast the spell");
+		if (FightManager.INSTANCE.getCurrentFighter(player.worldObj, FightHelper.getFightId(player)) != player) {
+			throw new RuntimeException("The entity " + player + " can't cast a spell, it's not his turn");
 		}
+
+		FightUtil.tryCastSpell(player, message.targetPosition);
 
 		return null;
 	}
