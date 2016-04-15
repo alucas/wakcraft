d4f8931b9bbe301b69ea86830f75083d910ad7a9
Antoine Lucas <git.alucas@gmail.com>
2016-04-15 18:53:47 +0200

    Clean InventoryProperty

--- src/main/java/heero/mc/mod/wakcraft/entity/property/InventoryProperty.java
+++ src/main/java/heero/mc/mod/wakcraft/entity/property/InventoryProperty.java
@@ -13,60 +13,60 @@ import net.minecraftforge.common.IExtendedEntityProperties;
 public class InventoryProperty implements IExtendedEntityProperties, ISynchProperties {
     public static final String IDENTIFIER = Reference.MODID + "Inventory";
 
-    protected static String TAG_WINVENTORY = "WIventory";
-    protected static String TAG_ARMOR = "Armor";
+    protected static String TAG_WAKCRAFT_INVENTORY = "WInventory";
+    protected static String TAG_ARMOR_INVENTORY = "Armor";
     protected static String TAG_ARMOR_ITEMS = "Items";
-    protected static String TAG_SLOT = "Slot";
+    protected static String TAG_SLOT_ID = "SlotId";
 
-    protected IInventory inventoryArmors;
+    protected IInventory armorInventory;
 
     @Override
     public void init(Entity entity, World world) {
-        inventoryArmors = new InventoryArmors(entity);
+        armorInventory = new InventoryArmors(entity);
     }
 
     @Override
     public void saveNBTData(NBTTagCompound tagRoot) {
-        NBTTagCompound tagInventory = new NBTTagCompound();
+        final NBTTagCompound tagInventory = new NBTTagCompound();
+        final NBTTagCompound tagArmorInventory = new NBTTagCompound();
+        final NBTTagList tagArmorItems = new NBTTagList();
 
-        NBTTagCompound tagArmor = new NBTTagCompound();
-        NBTTagList tagAmorsItems = new NBTTagList();
-
-        for (int i = 0; i < inventoryArmors.getSizeInventory(); i++) {
-            if (inventoryArmors.getStackInSlot(i) == null) {
+        for (int i = 0; i < armorInventory.getSizeInventory(); i++) {
+            final ItemStack armorItem = armorInventory.getStackInSlot(i);
+            if (armorItem == null) {
                 continue;
             }
 
-            NBTTagCompound tagItem = new NBTTagCompound();
-            inventoryArmors.getStackInSlot(i).writeToNBT(tagItem);
-            tagItem.setInteger(TAG_SLOT, i);
+            final NBTTagCompound tagArmorItem = new NBTTagCompound();
+            armorItem.writeToNBT(tagArmorItem);
+            tagArmorItem.setInteger(TAG_SLOT_ID, i);
 
-            tagAmorsItems.appendTag(tagItem);
+            tagArmorItems.appendTag(tagArmorItem);
         }
 
-        tagArmor.setTag(TAG_ARMOR_ITEMS, tagAmorsItems);
-        tagInventory.setTag(TAG_ARMOR, tagArmor);
-        tagRoot.setTag(TAG_WINVENTORY, tagInventory);
+        tagArmorInventory.setTag(TAG_ARMOR_ITEMS, tagArmorItems);
+        tagInventory.setTag(TAG_ARMOR_INVENTORY, tagArmorInventory);
+        tagRoot.setTag(TAG_WAKCRAFT_INVENTORY, tagInventory);
     }
 
     @Override
     public void loadNBTData(NBTTagCompound tagRoot) {
-        NBTTagCompound tagInventory = tagRoot.getCompoundTag(TAG_WINVENTORY);
-        NBTTagCompound tagArmor = tagInventory.getCompoundTag(TAG_ARMOR);
-        NBTTagList tagArmorItems = tagArmor.getTagList(TAG_ARMOR_ITEMS, 10);
+        final NBTTagCompound tagInventory = tagRoot.getCompoundTag(TAG_WAKCRAFT_INVENTORY);
+        final NBTTagCompound tagArmorInventory = tagInventory.getCompoundTag(TAG_ARMOR_INVENTORY);
+        final NBTTagList tagArmorItems = tagArmorInventory.getTagList(TAG_ARMOR_ITEMS, 10);
 
         for (int i = 0; i < tagArmorItems.tagCount(); i++) {
-            NBTTagCompound tagItem = tagArmorItems.getCompoundTagAt(i);
-            Integer slot = tagItem.getInteger(TAG_SLOT);
-            ItemStack stack = ItemStack.loadItemStackFromNBT(tagItem);
+            final NBTTagCompound tagArmorItem = tagArmorItems.getCompoundTagAt(i);
+            final Integer slotId = tagArmorItem.getInteger(TAG_SLOT_ID);
+            final ItemStack armorItem = ItemStack.loadItemStackFromNBT(tagArmorItem);
 
-            inventoryArmors.setInventorySlotContents(slot, stack);
+            armorInventory.setInventorySlotContents(slotId, armorItem);
         }
     }
 
     @Override
     public NBTTagCompound getClientPacket() {
-        NBTTagCompound tagRoot = new NBTTagCompound();
+        final NBTTagCompound tagRoot = new NBTTagCompound();
 
         saveNBTData(tagRoot);
 
@@ -78,7 +78,7 @@ public class InventoryProperty implements IExtendedEntityProperties, ISynchPrope
         loadNBTData(tagRoot);
     }
 
-    public IInventory getInventoryArmors() {
-        return inventoryArmors;
+    public IInventory getArmorInventory() {
+        return armorInventory;
     }
 }
