02394080d3e60783f6d169aa0d915c551a50749b
Antoine Lucas <git.alucas@gmail.com>
2016-04-16 12:50:28 +0200

    Make BlockPlant extend of BlocOverSlab

--- src/main/java/heero/mc/mod/wakcraft/block/BlockPlant.java
+++ src/main/java/heero/mc/mod/wakcraft/block/BlockPlant.java
@@ -2,23 +2,17 @@ package heero.mc.mod.wakcraft.block;
 
 import heero.mc.mod.wakcraft.creativetab.WCreativeTabs;
 import net.minecraft.block.material.Material;
-import net.minecraft.block.properties.PropertyInteger;
-import net.minecraft.block.state.BlockState;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.util.BlockPos;
 import net.minecraft.util.EnumWorldBlockLayer;
-import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 
-public class BlockPlant extends BlockGenericTransparent {
-    public static final PropertyInteger PRO_BOTTOM_POSITION = PropertyInteger.create("bottom_position", 0, 3);
-
+public class BlockPlant extends BlockOverSlab {
     public BlockPlant(Material material) {
         super(material, WCreativeTabs.tabBlock);
 
         setFullBlock(false);
-        setDefaultState(blockState.getBaseState().withProperty(PRO_BOTTOM_POSITION, 0));
     }
 
     public AxisAlignedBB getCollisionBoundingBox(World worldIn, BlockPos pos, IBlockState state) {
@@ -26,27 +20,12 @@ public class BlockPlant extends BlockGenericTransparent {
     }
 
     @Override
-    protected BlockState createBlockState() {
-        return new BlockState(this, PRO_BOTTOM_POSITION);
-    }
-
-    @Override
-    public IBlockState getActualState(IBlockState state, IBlockAccess worldIn, BlockPos pos) {
-        IBlockState stateDown = worldIn.getBlockState(pos.down());
-        if (!(stateDown.getBlock() instanceof BlockSlab)) {
-            return state.withProperty(PRO_BOTTOM_POSITION, 0);
-        }
-
-        return state.withProperty(PRO_BOTTOM_POSITION, 4 - BlockSlab.getTopPosition(stateDown));
-    }
-
-    @Override
-    public int getMetaFromState(IBlockState state) {
-        return 0;
-    }
-
-    @Override
     public EnumWorldBlockLayer getBlockLayer() {
         return EnumWorldBlockLayer.CUTOUT;
+    }
+
+    @Override
+    public float getBlockHeight() {
+        return 1;
     }
 }
